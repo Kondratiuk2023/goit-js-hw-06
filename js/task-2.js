eee84b03b50a87aac1db59e48048c044edc22d81
Kondratiuk2023 <149534389+Kondratiuk2023@users.noreply.github.com>
2024-03-09 23:10:45 +0200

    Update task-2,3

--- js/task-2.js
+++ js/task-2.js
@@ -1,21 +1,23 @@
 class Storage {
+  #items;
   constructor(items) {
-    this._items = items;
+    this.#items = items;
   }
 
   getItems() {
-    return this._items;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this._items.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    const itemIndex = this._items.indexOf(itemToRemove);
-    if (itemIndex !== -1) {
-      this._items.splice(itemIndex, 1);
-    }
+    // const itemIndex = this.#items.indexOf(itemToRemove);
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+    // if (itemIndex !== -1) {
+    //   this.#items.splice(itemIndex, 1);
+    // }
   }
 }
 
--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,24 @@
 class StringBuilder {
+    #value;
+    
     constructor(initialValue) {
-        this.value = initialValue;
+        this.#value = initialValue;
     }
 
     getValue() {
-        return this.value;
+        return this.#value;
     }
 
     padEnd(str) {
-        this.value += str;
+        this.#value += str;
     }
 
     padStart(str) {
-        this.value = str + this.value;
+        this.#value = str + this.#value;
     }
 
     padBoth(str) {
-        this.value = str + this.value + str;
+        this.#value = str + this.#value + str;
     }
 }
 
@@ -28,3 +30,5 @@ builder.padEnd("^");
 console.log(builder.getValue()); // "^.^"
 builder.padBoth("=");
 console.log(builder.getValue()); // "=^.^="
+
+
